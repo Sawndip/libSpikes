6396005dfbe9dc90c503a8243ddebd1d54ee640f
Akshay Raj Gollahalli <akshay@gollahalli.com>
2017-12-28 18:44:04 +1300

    using const for the loop

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main(){
 
     auto a = FileReader(R"(C:\Users\aksha\Documents\GitHub\NeuCube_Matlab\data\wrist_movement_eeg)");
 
-    for (auto i:a.get_data()) {
+    for (const auto &i:a.get_data()) {
         std::cout<<i<<'\n';
     }
 
